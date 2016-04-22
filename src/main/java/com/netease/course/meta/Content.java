5c0a0d1664107b69a720d5b05db39687ef37766b
SH <shihuemail@qq.com>
2016-04-22 14:29:24 +0800

    ficommit

--- src/main/java/com/netease/course/meta/Content.java
+++ src/main/java/com/netease/course/meta/Content.java
@@ -5,9 +5,9 @@ import java.io.UnsupportedEncodingException;
 public class Content {
 	private int id;
 	private int price;
-	private String title;
-	private String icon;
-	private String cabstract;
+	private byte[] title;
+	private byte[] icon;
+	private byte[] cabstract;
 	private byte[] text;
 	public int getId() {
 		return id;
@@ -21,22 +21,22 @@ public class Content {
 	public void setPrice(int price) {
 		this.price = price;
 	}
-	public String getTitle() {
-		return title;
+	public String getTitle() throws UnsupportedEncodingException {
+		return new String(title, "UTF-8");
 	}
-	public void setTitle(String title) {
+	public void setTitle(byte[] title) {
 		this.title = title;
 	}
-	public String getIcon() {
-		return icon;
+	public String getIcon() throws UnsupportedEncodingException {
+		return new String(icon,"UTF-8");
 	}
-	public void setIcon(String icon) {
+	public void setIcon(byte[] icon) {
 		this.icon = icon;
 	}
-	public String getCabstract() {
-		return cabstract;
+	public String getCabstract() throws UnsupportedEncodingException {
+		return new String(cabstract,"UTF-8");
 	}
-	public void setCabstract(String cabstract) {
+	public void setCabstract(byte[] cabstract) {
 		this.cabstract = cabstract;
 	}
 	public String getText() throws UnsupportedEncodingException {
